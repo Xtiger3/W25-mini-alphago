9b0d9f8495c2899ea39e6ee8ea8aaf5bc1b6659a
Jakedog <jakedog228@gmail.com>
2025-02-02 14:02:43 -0500

    implemented the ResBlock according to the paper

--- network.py
+++ network.py
@@ -19,7 +19,32 @@ class ConvBlock(nn.Module):
         out = self.relu(out)
 
         return out
-    
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel: int, stride: int):
+        super().__init__()
+
+        padding = (kernel-1) // 2
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding)
+        self.norm1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding)
+        self.norm2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        """ based on: https://discovery.ucl.ac.uk/id/eprint/10045895/1/agz_unformatted_nature.pdf#page=27 """
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.norm2(out)
+        out = out + x
+        out = self.relu(out)
+
+        return out
 
 
 if __name__ == "__main__":

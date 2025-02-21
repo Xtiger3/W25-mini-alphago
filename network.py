4aa7911b5889682d707bab33a67acf4af9fe18e0
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2025-02-21 12:42:34 -0500

    network.py templates

--- network.py
+++ network.py
@@ -1,14 +1,16 @@
 import torch.nn as nn
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel: int, stride: int):
+    def __init__(self, in_channels: int, out_channels: int, kernel: int):
         super().__init__()
-
-        assert(kernel % 2 == 1) 
-
-        padding = (kernel-1) // 2
     
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel, stride, padding)
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel,
+            stride=1,
+            padding="same"
+        )
         self.norm = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
     
@@ -21,9 +23,84 @@ class ConvBlock(nn.Module):
         return out
     
 
+class ResBlock(nn.Module):
+    def __init__(self, channels: int, kernel: int):
+        super().__init__()
+
+        conv_kwargs = {
+            "in_channels": channels,
+            "out_channels": channels,
+            "kernel_size": kernel,
+            "stride": 1,
+            "padding": "same"
+        }
+
+        self.conv1 = nn.Conv2d(**conv_kwargs)
+        self.norm1 = nn.BatchNorm2d(channels)
+
+        self.conv2 = nn.Conv2d(**conv_kwargs)
+        self.norm2 = nn.BatchNorm2d(channels)
+
+        self.relu = nn.ReLU()
+    
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.norm2(out)
+        
+        out += x
+
+        out = self.relu(out)
+
+        return out
+
+
+class PolicyHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Initialize members
+
+        raise NotImplementedError()
+    
+
+    def forward(self):
+        # Return tensor in the shape of (batch, 9*9 + 1)
+        raise NotImplementedError()
+
+
+class ValueHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Initialize members
+
+        raise NotImplementedError()
+    
+
+    def forward(self):
+        # Return tensor in the shape of (batch, 1)
+        raise NotImplementedError()
+
+
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Initialize members
+
+        raise NotImplementedError()
+    
+
+    def forward(self):
+        # Return tuple of tensors in the shape ((batch, 9*9 + 1), (batch, 1))
+
+        raise NotImplementedError()
+
 
 if __name__ == "__main__":
-    conv_nn = ConvBlock(9, 1, 3, 1) 
+    model = NeuralNet()
 
     #TODO: Add code for creating child node of the board, and pass the tensor generated from it through the network
 

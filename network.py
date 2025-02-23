066fe2bcc40b1edf68f835eacd9a144fe2615cd4
MaxWangZY <zmaxwang@umich.edu>
2025-02-23 13:01:31 -0500

    policy head and value head

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel: int):
@@ -60,43 +61,96 @@ class ResBlock(nn.Module):
 
 
 class PolicyHead(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels: int, board_size=9):
         super().__init__()
-        # Initialize members
+        self.board_size = board_size
 
-        raise NotImplementedError()
-    
+        # 1. A convolution of 2 filters of kernel size 1 ×1 with stride 1
+        # input channels -> 2 channels
+        self.conv = nn.Conv2d(in_channels, out_channels=2, kernel_size=1, stride=1)
 
+        # 2. Batch normalisation
+        self.bn = nn.BatchNorm2d(2)
+
+        # 3. A rectifier non-linearity
+        self.relu = nn.ReLU()
+
+        # 4. A fully connected linear layer, 
+        # corresponding to logit probabilities for all intersections and the pass move
+        self.fc = nn.Linear(2 * board_size * board_size, board_size * board_size + 1)
+        
     def forward(self, x):
-        # Return tensor in the shape of (batch, 9*9 + 1)
-        raise NotImplementedError()
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        # flatten to (batch_size, 2*board_size*board_size)
+        x = x.view(x.size(0), -1)
+        
+        return self.fc(x)
 
 
 class ValueHead(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels: int, board_size=9):
         super().__init__()
-        # Initialize members
+        self.board_size = board_size
 
-        raise NotImplementedError()
-    
+        # 1. A convolution of 1 filter of kernel size 1 ×1 with stride 1
+        self.conv = nn.Conv2d(in_channels, out_channels=1, kernel_size=1, stride=1)
+
+        # 2. Batch Normalisation on the single channel output
+        self.bn = nn.BatchNorm2d(1)
+
+        # 3. A rectifier non-linearity
+        self.relu = nn.ReLU()
+
+        # 4. A fully connected linear layer to a hidden layer of size 256
+        self.fc = nn.Linear(board_size * board_size, 256)
+
+        # 5. ReLu again
+
+        # 6. A fully connected linear layer to a scalar
+        self.fc2 = nn.Linear(256, 1)
 
     def forward(self, x):
-        # Return tensor in the shape of (batch, 1)
-        raise NotImplementedError()
+        # 1.
+        x = self.conv(x)
+        # 2. 
+        x = self.bn(x)
+        # 3.
+        x = self.relu(x)
+
+        # Flatten
+        x = x.view(x.size(0), -1)
+
+        # 4.
+        x = self.fc(x)
+        # 5.
+        x = self.relu(x)
+        # 6.
+        x = self.fc2(x)
+        # 7. Tanh non-linearity to ensure output is in [-1, 1]
+        return torch.tanh(x)
+
 
 
 class NeuralNet(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels: int, out_channels: int, kernel: int, stride: int, num_residuals=19):
         super().__init__()
-        # Initialize members
 
-        raise NotImplementedError()
-    
+        assert(kernel % 2 == 1) 
 
+        self.conv = ConvBlock(in_channels, out_channels, kernel, stride)
+        
+        self.residuals = nn.Sequential(
+            *[ResBlock(out_channels, out_channels, kernel, stride) 
+              for _ in range(num_residuals)]
+        )
+        
     def forward(self, x):
-        # Return tuple of tensors in the shape ((batch, 9*9 + 1), (batch, 1))
+        x = self.conv(x)
+        x = self.residuals(x)
 
-        raise NotImplementedError()
+        return x
 
 
 if __name__ == "__main__":

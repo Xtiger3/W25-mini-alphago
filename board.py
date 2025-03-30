0ab00a2df574debebacf38c9947490f7075316db
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2025-03-30 13:22:23 -0400

    Fix incorrect pass counting

--- board.py
+++ board.py
@@ -356,7 +356,7 @@ class Board:
                 self.move += 1
             
             return True
-        else:
+        elif move:
             self.num_passes = 0
 
         # Prohibit placing stone on top of another
